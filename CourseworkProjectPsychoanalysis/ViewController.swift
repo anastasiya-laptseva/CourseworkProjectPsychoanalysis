884d56a8ca149d2ee09de7d5892b0c260f12f721
Anastasiya Lapteva <anas.alex.novikova@gmail.com>
2020-03-26 13:49:15 +0300

    start project

---
+++ CourseworkProjectPsychoanalysis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CourseworkProjectPsychoanalysis
+//
+//  Created by Anastasiya Laptseva on 3/24/20.
+//  Copyright © 2020 Anastasiya Laptseva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
