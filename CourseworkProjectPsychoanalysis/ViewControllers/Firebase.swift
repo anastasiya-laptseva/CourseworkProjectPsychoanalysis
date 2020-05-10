0d92ec49649e3d74918be6b11ce63f939d04885f
Anastasiya Lapteva <anas.alex.novikova@gmail.com>
2020-05-11 00:43:45 +0300

    Fix

---
+++ CourseworkProjectPsychoanalysis/ViewControllers/Firebase.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  CourseworkProjectPsychoanalysis
+//
+//  Created by Anastasiya Laptseva on 5/8/20.
+//  Copyright © 2020 Anastasiya Laptseva. All rights reserved.
+//
+//
+//import UIKit
+//import FirebaseAuth
+//
+//class ViewController: UIViewController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//        
+//        let email = "anas.alex.novikova@gmail.com"
+//        let password = "123456"
+//        Auth.auth().createUser(withEmail: email, password: password) {
+//            authResult, error in
+//            // ...
+//            print(authResult)
+//        }
+//        
+//        Auth.auth().currentUser
+//        print(Auth.auth().currentUser?.uid)
+//        do {
+//            try Auth.auth().signOut()
+//        } catch {
+//            print(error)
+//        }
+//        
+//        Auth.auth().signInAnonymously {
+//            authResult, error in
+//            // ...
+//        }
+//        
+//    }
+//    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+//}
--- Podfile
+++ Podfile
@@ -8,4 +8,5 @@ target 'CourseworkProjectPsychoanalysis' do
   # Pods for CourseworkProjectPsychoanalysis
 
 pod 'SwiftLint' 
+pod 'Firebase/Auth'
 end
